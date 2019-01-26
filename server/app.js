27ab648ef9e0e9e36073e7ddc6e31ae479c0f188
Eric Kulchycki <erickulchycki@gmail.com>
2019-01-26 15:08:56 -0600

    Got server working

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
+
+
 var createError = require('http-errors');
-var express = require('express');
+var express = require('express'); 
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,19 +10,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +28,11 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log('RESTful API server started on: ' + port);
---
+++ server/server.js
@@ -0,0 +1,5 @@
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
