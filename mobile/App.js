593a1f184ad32dba8f9eb1f542faeb63619d8708
Jayde Erasmo <jaydeerasmo@gmail.com>
2019-01-30 01:04:07 -0600

    Added initial Mobile files to Repo

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, Image } from 'react-native';
+
+
+
+export default class App extends Component {
+    render() {
+      return (
+        <View>
+
+        </View>
+      );
+    }
+}
