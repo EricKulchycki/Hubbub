6fae2b4da1b0d1099458684404850a47d7ebabec
Eric Kulchycki <erickulchycki@gmail.com>
2019-02-06 14:57:50 -0600

    Started working on posts for backend

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,26 @@
 module.exports = (app, db) => {
-  app.get( "/sayHello", (req, res) => {
-	console.log("Requested say hello");
-	res.send('Hello from the back-end.');
-  });
-  
-  app.get("/user/:id", (req, res) => 
-  	db.user.findOne({
-  		where: {
-  			id: req.params.id
-  		}
-  	}).then( (result) => res.json(result) )
-  );
+		app.get( "/sayHello", (req, res) => {
+		console.log("Requested say hello");
+		res.send('Hello from the back-end.');
+		});
+		
+		app.get("/user/:id", (req, res) => 
+			db.user.findOne({
+				where: {
+					id: req.params.id
+				}
+			}).then( (result) => res.json(result) )
+		);
+
+		//create post will have different values send in the message body
+		//should be sent in JSON, then must parse and store in db
+		app.put("/createpost/", (req, res) => 
+			db.posts.store({
+				
+			});	
+		);
+	
+
+
+
 }
