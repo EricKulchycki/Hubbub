6ce018d51e335ea35dedb57240b27c215bd21b99
Jayde Erasmo <jaydeerasmo@gmail.com>
2019-02-10 13:19:17 -0600

    dependency for mobile

---
+++ mobile/navigation/AppNavigator.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import HomeScreen from '../screens/HomeScreen'
+import SignInScreen from '../screens/SignInScreen'
+import AuthLoadingScreen from '../screens/AuthLoadingScreen'
+import { createAppContainer, createStackNavigator, createSwitchNavigator } from 'react-navigation';
+
+const AppStack = createStackNavigator({Home: HomeScreen});
+const AuthStack = createStackNavigator({SignIn: SignInScreen}, {headerMode: 'screen'});
+export default createAppContainer(createSwitchNavigator({
+  // You could add another route here for authentication.
+  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+  AuthLoading: AuthLoadingScreen,
+  App: AppStack,
+  Auth: AuthStack
+},
+{
+  initialRouteName: 'Auth',
+}
+));
