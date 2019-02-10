eca3a0d8872302e4c1802a5a234393f8d0f6f943
Ericson Atanacio <ericson_atanacio@hotmail.com>
2019-02-10 13:44:08 -0600

    Added 'New Post' screen

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is a sample React application with a simple back-end NodeJS application.
 Node modules used: npm install --save
 - react-native-elements
 - react-native-searchable-dropdown
+- react-native-vector-icons
--- mobile/screens/HomeScreen.js
+++ mobile/screens/HomeScreen.js
@@ -7,10 +7,14 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Modal,
+  TouchableHighlight,
+  Picker,
+  TextInput
 } from 'react-native';
 
 import SearchableDropdown from 'react-native-searchable-dropdown';
-import {Header} from 'react-native-elements';
+import {Header, Icon, Button, Rating, CheckBox} from 'react-native-elements';
 
 import { WebBrowser } from 'expo';
 
@@ -20,18 +24,45 @@ var items = [
   {id: 1, name:'Eric',}, {id:2, name:'Ericson',}, {id:3, name:'John',}, {id:4, name:'test4',},
 ];
 
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
+  state = {
+    modalVisible: false,
+    pickerSelection: '',
+    nameOfMedia: '',
+    checked: false,
+    details: '',
+  };
+
+  setModalVisible(visible){
+    this.setState({modalVisible: visible});
+  }
+
+  ratingCompleted(rating){
+    alert(rating)
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Header backgroundColor = "#FF0000"
-          leftComponent={{icon: 'menu', color: '#fff'}}
-          rightComponent={{icon: 'home', color: '#fff'}}
+          leftComponent={
+            <Icon
+              containerStyle={{right: 37}}
+              reverse
+              name='alpha-h'
+              type='material-community'
+              size={50}
+              color='#FF0000'
+            />
+          }
+          //rightComponent={{icon: 'add-circle', color: '#fff'}}
         />
+
 
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
 
@@ -63,6 +94,101 @@ export default class HomeScreen extends React.Component {
           // defaultIndex={2}
           placeholder="Search"
           underlineColorAndroid="transparent"
+        />
+
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            alert('Modal has been closed.');
+            this.setModalVisible(!this.state.modalVisible);
+          }}>
+          <ScrollView>
+            <View style={{paddingHorizontal: 5, marginTop: 22}}>
+              <View>
+                <Text style = {{fontWeight: 'bold', fontSize: 34}}>
+                New Post
+                </Text>
+
+                <Text style = {{paddingTop: 10, fontSize: 22}}>
+                Category
+                </Text>
+                <Picker
+                  selectedValue={this.state.pickerSelection}
+                  onValueChange={(itemValue, itemIndex) =>
+                    this.setState({pickerSelection: itemValue})
+                  }>
+                  <Picker.Item label="Category 1" value="cat1"/>
+                  <Picker.Item label="Category 2" value="cat2"/>
+                  <Picker.Item label="Category 3" value="cat3"/>
+                </Picker>
+
+                <Text style = {{paddingTop: 10, fontSize: 22}}>
+                Name of media
+                </Text>
+                <TextInput
+                  style = {{paddingHorizontal: 5, borderColor: 'gray', borderWidth: 1}}
+                  onChangeText={(nameOfMedia) => this.setState({nameOfMedia})}
+                  value={this.state.nameOfMedia}
+                />
+
+                <Text style = {{paddingTop: 10, fontSize: 22}}>
+                Rating
+                </Text>
+                <Rating
+                  onFinishRating={this.ratingCompleted}
+                />
+                <CheckBox
+                  title='Disable Rating'
+                  checked={this.state.checked}
+                  onPress={() => this.setState({checked: !this.state.checked})}
+                />
+
+                <Text style = {{paddingTop: 10, fontSize: 22}}>
+                Details
+                </Text>
+                <TextInput
+                  multiline={true}
+                  textAlignVertical= 'top'
+                  scrollEnabled={true}
+                  numberOfLines={4}
+                  style = {{paddingHorizontal: 5, borderColor: 'gray', borderWidth: 1}}
+                  onChangeText={(details) => this.setState({details})}
+                  value={this.state.details}
+                />
+
+                <TouchableHighlight
+                  style={{position: 'relative', left: 340}} 
+                  onPress={() => {
+                    this.setModalVisible(!this.state.modalVisible);
+                  }}>
+                  <Icon
+                    reverse
+                    name='add-circle'
+                    type='material'
+                    size={24}
+                    color='#FF0000'/>
+                </TouchableHighlight>
+
+              </View>
+            </View>
+          </ScrollView>
+        </Modal>
+
+        <Button
+          buttonStyle={{bottom: 5, left: 355, height: 50, width: 50, borderRadius: 25}}
+          icon={
+            <Icon
+              reverse
+              name='add-circle'
+              type='material'
+              size={24}
+              color='#FF0000'/>
+          }
+          onPress={() => {
+            this.setModalVisible(true);
+          }}
         />
 
       </View>
