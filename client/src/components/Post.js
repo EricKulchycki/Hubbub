4cc6fe9629647130b082efa5d7d9a00ae48341a2
GershonR <reydmang@myumanitoba.ca>
2019-02-16 15:06:21 -0600

    Initial Commit

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
+import '../css/Post.css';
+import '../css/Application.css';
 
 class Post extends Component {
-  getStyle = () => {
-    return {
-      padding: '5px',
-      border: '3px solid #ffffff'
-     
-    }
-  }
 
   render() {
     return  (
-      <div style={this.getStyle()}>
-      <h1 style={{fontSize:'16px', textAlign: 'left', margin: '0%'}}> {this.props.post.user.username} </h1>
-      <h2  style={{fontSize:'13px', textAlign: 'left', margin: '0%'}}>{this.props.post.title} </h2>
-       <h3  style={{fontSize:'12px', textAlign: 'left', margin: '0%'}} >Category: {this.props.post.category}</h3>
-        <p  style={{fontSize:'12px'}} >{this.props.post.body}</p>
-        </div>
+      <div className="post-style">
+		<h1 className="post-username text-left margin-none"> {this.props.post.user.username} </h1>
+		<h2 className="post-title text-left margin-none">{this.props.post.title} </h2>
+		<h3 className="post-category text-left margin-none">Category: {this.props.post.category}</h3>
+        <p className="post-body">{this.props.post.body}</p>
+       </div>
     );
   }
 }
