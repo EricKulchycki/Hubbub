48f8787ed7bf3e739d51e04baba676478479d20f
GershonR <reydmang@myumanitoba.ca>
2019-02-16 15:23:49 -0600

    Some CSS changes

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -7,8 +7,9 @@ class Post extends Component {
   render() {
     return  (
       <div className="post-style">
-		<h1 className="post-username text-left margin-none"> {this.props.post.user.username} </h1>
-		<h2 className="post-title text-left margin-none">{this.props.post.title} </h2>
+		<hr></hr>
+		<h1 className="post-username text-left margin-none">{this.props.post.user.firstName} {this.props.post.user.lastName}</h1>
+		<h2 className="post-title text-left margin-none">{this.props.post.title}</h2>
 		<h3 className="post-category text-left margin-none">Category: {this.props.post.category}</h3>
         <p className="post-body">{this.props.post.body}</p>
        </div>
