910fc127694faec000d6990658602c1dff610d8f
Gershon <reydmang@myumanitoba.ca>
2019-02-22 12:04:20 -0600

    Delete friend endpoint

--- server/routes/friend.js
+++ server/routes/friend.js
@@ -11,8 +11,23 @@ module.exports = (app, db) => {
 		console.log("Requested new friend creation");
 		console.log(req.body);
 		db.friend.create({
-		userId: req.body.userId,
-		friendId: req.body.friendId
+			userId: req.body.userId,
+			friendId: req.body.friendId
+		}).then( (result) => res.json(result) );
+	});
+
+	app.post("/api/v1/friend/delete", (req, res) => {
+		console.log("Requested friend deletion");
+		console.log(req.body);
+		if (!req.body.userId || !req.body.friendId) {
+			res.send("Missing userId or friendId");
+			return;
+		}
+		db.friend.destroy({
+			where: {
+				userId: req.body.userId,
+				friendId: req.body.friendId
+			}
 		}).then( (result) => res.json(result) );
 	});
 }
