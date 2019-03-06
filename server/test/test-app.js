56e038b7fc7bd4fc10f799aea82931c393bfb4d6
Michael Reimer <themichaelreimer@gmail.com>
2019-03-06 17:26:09 -0600

    Basic endpoint tests

--- server/test/test-app.js
+++ server/test/test-app.js
@@ -8,16 +8,98 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
+function validUser( res ){
+	res.should.have.property('id');
+    res.should.have.property('username');
+    res.should.have.property('password');
+    res.should.have.property('email');
+    res.should.have.property('firstName');
+    res.should.have.property('lastName');
+}
+
+function validPost( res ){
+	res.should.have.property('id');
+	res.should.have.property('category');
+	res.should.have.property('title');
+	res.should.have.property('body');
+	//Rating is allowed to be null, do not check
+	res.should.have.property('userId');
+} 
+
+function printObj( obj ){
+	var ret = "";
+	for( var prop in obj){
+		ret += prop + ": "+ obj[prop]+': ';
+	}
+	alert(ret);
+}
 
 describe('API Endpoints', function() {
-  it('should return user with specified id GET');
-});
-
-it('should return user with specified id GET', function(done) {
+  it('should return user with specified id GET', function(done) {
     chai.request(server)
       .get('/api/v1/user/1')
       .end(function(err, res){
         res.should.have.status(200);
+        res = res.body
+
+        validUser(res)
+
         done();
       });
   });
+
+  //Create user unit test
+  //after alt database is ready
+
+  it('should return list of all users', function(done){
+  	chai.request(server)
+  		.post('/api/v1/user/list')
+  		.send({'firstName': ''})
+  		.end(function(err,res){
+  			res.should.have.status(200);
+  			res = res.body
+  			res.should.be.a('array');
+  			done();
+  		});
+  });
+
+  it('should get all friends of user', function(done){
+  	chai.request(server)
+  		.get('/api/v1/friend/1')
+  		.end(function(err,res){
+  			res.should.have.status(200);
+  			res = res.body
+
+  			res.should.be.a('array');
+  			done();
+  		});
+  });  
+
+  //"Create a friendship" - (User follows another)
+
+  //Delete a friendship
+
+  //Get all posts from a category
+
+  it('Get a particular post with an id', function(done){
+  	chai.request(server)
+  		.get('/api/v1/post/1')
+  		.end(function(err,res){
+  			res.should.have.status(200);
+  			res = res.body
+
+  			//printObj(res)
+
+  			validPost(res)
+  			done();
+  		});
+  });
+
+  //Create a post
+
+  //Delete a post
+
+  //Get all posts from a friend
+
+});
+
