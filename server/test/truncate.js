a69bf83a7cf1cc4f8ad668e44849c481a93ca29c
Eric Kulchycki <erickulchycki@gmail.com>
2019-03-13 10:09:47 -0500

    One more change with regards to reseting test db

--- server/test/truncate.js
+++ server/test/truncate.js
@@ -6,7 +6,7 @@ module.exports = async function truncate() {
   return await Promise.all(
     map(Object.keys(models), (key) => {
       if (['sequelize', 'Sequelize'].includes(key)) return null;
-      return models[key].destroy({ where: {}, force: true, restartIdentity: true });
+      return models[key].destroy({ where: {}, force: true, truncate : true, cascade: false });
     })
   );
 }
