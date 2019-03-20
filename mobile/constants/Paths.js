4023240117269bea5f1a59251edef7cc57b573ba
Jayde Erasmo <jaydeerasmo@gmail.com>
2019-03-19 20:38:18 -0500

    default user image URI

--- mobile/constants/Paths.js
+++ mobile/constants/Paths.js
@@ -1,6 +1,7 @@
 
   export const SERVER = "http://hubbub.gersh.in:4000";
   export const AUTH = "/google?socketId=";
+  export const DEFAULT_USER = "https://image.flaticon.com/icons/png/512/21/21294.png";
   export const getFriendsPosts = "/api/v1/posts/allFriends/";
   export const createPost = '/api/v1/post/create';
   export const getPostByID = '/api/v1/post/';
@@ -8,4 +9,6 @@
   export const getFriends = '/api/v1/friend/';
   export const updateUser = '/api/v1/user/update';
   export const addFriend = '/api/v1/friend/create';
+  export const removeFriend = '/api/v1/friend/delete';
+  export const getAllUsers = '/api/v1/user/list';
   export const domain = 'http://142.93.147.148:4000';
