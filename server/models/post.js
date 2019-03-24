382f5551ac9a8f5e2d4e74643fedffac19e20b45
Eric Kulchycki <erickulchycki@gmail.com>
2019-03-24 14:56:47 -0500

    Oh boy this might cause some problems.

--- server/models/post.js
+++ server/models/post.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
 	    title: DataTypes.STRING,
       rating: DataTypes.INTEGER,
       body: DataTypes.STRING,
+      spoiler: DataTypes.BOOLEAN,
     },
     {
       freezeTableName: true,
