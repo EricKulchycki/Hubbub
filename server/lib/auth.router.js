c67b0369707c86a66cb919e06d983d6b935f0d7e
laurenpenner <penner34@umanitoba.ca>
2019-03-24 19:13:31 -0500

    bin, config and lib files abid by conventions

--- server/lib/auth.router.js
+++ server/lib/auth.router.js
@@ -4,30 +4,24 @@ const passport = require('passport')
 const authController = require('./auth.controller')
 
 // Setting up the passport middleware for each of the OAuth providers
-const twitterAuth = passport.authenticate('twitter')
+//const twitterAuth = passport.authenticate('twitter')
 const googleAuth = passport.authenticate('google', { scope: ['profile', 'email'] })
-const facebookAuth = passport.authenticate('facebook')
+//const facebookAuth = passport.authenticate('facebook')
 const githubAuth = passport.authenticate('github')
 
 // Routes that are triggered by the callbacks from each OAuth provider once 
 // the user has authenticated successfully
-router.get('/twitter/callback', twitterAuth, authController.twitter)
 router.get('/google/callback', googleAuth, authController.google)
-router.get('/facebook/callback', facebookAuth, authController.facebook)
-router.get('/github/callback', githubAuth, authController.github)
+
 
 // This custom middleware allows us to attach the socket id to the session
 // With that socket id we can send back the right user info to the right 
 // socket
 router.use((req, res, next) => {
-  req.session.socketId = req.query.socketId
-  next()
+	req.session.socketId = req.query.socketId
+	next()
 })
 
-// Routes that are triggered on the client
-router.get('/twitter', twitterAuth)
 router.get('/google', googleAuth)
-router.get('/facebook', facebookAuth)
-router.get('/github', githubAuth)
 
 module.exports = router
