5ba7e883991fb1ef099941ffc0ce1eb8dbc67254
Gershon <reydmang@myumanitoba.ca>
2019-03-24 22:11:52 -0500

    Remove console logs, code comments, fix indentation, remove SQL output from tests

--- server/models/friend.js
+++ server/models/friend.js
@@ -1,26 +1,26 @@
 module.exports = (sequelize, DataTypes) => {
-  const Friend = sequelize.define('friend', {
-    id: {
-			type: DataTypes.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		userId: DataTypes.INTEGER,
-    },
-    {
-      freezeTableName: true,
-	  	defaultScope: {
-				attributes: { exclude: ['id', 'updatedAt'] },
-	  	}
-    }
-  );
-  
-  Friend.associate = (models) => {
-		Friend.belongsTo(models.user, {
-			foreignKey: 'friendId',
-			constraints: false
-		});
-};
+    const Friend = sequelize.define('friend', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        userId: DataTypes.INTEGER,
+    }, {
+        freezeTableName: true,
+        defaultScope: {
+            attributes: {
+                exclude: ['id', 'updatedAt']
+            },
+        }
+    });
 
-  return Friend;
+    Friend.associate = (models) => {
+        Friend.belongsTo(models.user, {
+            foreignKey: 'friendId',
+            constraints: false
+        });
+    };
+
+    return Friend;
 }
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,94 +1,88 @@
 module.exports = (app, db) => {
 
-  const Op = db.sequelize.Op;
-  const friendModule = require("../modules/friends/friend");
-  
-  var validate = require('express-validation');
-  var validation = require('../validation/post.js');
+    const Op = db.sequelize.Op;
+    const friendModule = require("../modules/friends/friend");
 
-  //Get a post given an id
-  app.get("/api/v1/post/:id", (req, res) => {
-  	console.log("Requested post " + req.params.id);
-  	db.post.findOne({
-  		where: {
-  			id: req.params.id
-  		},
-	  	include: [db.user]
-  	}).then( (result) => res.json(result) );
-  });
+    var validate = require('express-validation');
+    var validation = require('../validation/post.js');
 
-  app.post("/api/v1/post/delete", (req,res) =>{
-    console.log("Requested post deletion: "+req.body.id);
-    
-    if( !req.body.id ){
-      res.send("Could not delete post! Missing ID");
-    }
+    //Get a post given an id
+    app.get("/api/v1/post/:id", (req, res) => {
+        db.post.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.user]
+        }).then((result) => res.json(result));
+    });
 
-    db.post.destroy({
-      where: {
-        id: req.body.id
-      }
-    }).then( (result) => res.json(result));
+    app.post("/api/v1/post/delete", (req, res) => {
+        if (!req.body.id) {
+            res.send("Could not delete post! Missing ID");
+        }
 
-  });
-  
-  //Create a new post
-  app.post("/api/v1/post/create", validate(validation), (req, res, next) => {
-    console.log("Requested new post creation");
-	console.log(req.body);
-    db.post.create({
-      title: req.body.title,
-      category: req.body.category,
-      body: req.body.body,
-      userId: req.body.userId,
-      rating: req.body.rating,
-	  spoiler: req.body.spoiler,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }).then( (result) => res.json(result) );
-  });
+        db.post.destroy({
+            where: {
+                id: req.body.id
+            }
+        }).then((result) => res.json(result));
 
-	//Get all posts of friends given a userId
-	app.get("/api/v1/posts/allFriends/:userId", (req, res) => {
-		let userId = req.params.userId;
-		console.log("Requested all posts from " + userId + "'s friends");
-		// Get the users friends, store the ids in a list, and then get a list of posts using those ids
-		friendModule.getFriends(app, db, userId).then( (friends) => {
-			if (!friends || !friends.length) {
-				res.json([]);
-				return;
-			}
-			var friendIds = friends.map(function(friend) {return friend.user.id;});
-			db.post.findAll({
-				where: {
-					userId: {
-						[Op.or]: friendIds
-					}
-				},
-				include: [db.user]
-			}).then( (result) => res.json(result) );
-		});
-   });
+    });
 
-  //Get all posts of a given category
-  app.get("/api/v1/posts/categories/:cat", (req, res) => {
-    console.log("Requested posts " + req.params.cat);
-  	db.post.findAll({
-  		where: {
-  			category: req.params.cat
-  		},
-	  	include: [db.user]
-  	}).then( (result) => res.json(result));
-	});
-	
-  //Get all posts for a specified user
-  app.get("/api/v1/posts/user/:id", (req, res) => {
-  	console.log("Requested posts for user ID " + req.params.id);
-  	db.post.findAll({
-  		where: {
-  			userId: req.params.id
-  		},
-	  	include: [db.user]
-  	}).then( (result) => res.json(result) );
-  });
+    //Create a new post
+    app.post("/api/v1/post/create", validate(validation), (req, res, next) => {
+        db.post.create({
+            title: req.body.title,
+            category: req.body.category,
+            body: req.body.body,
+            userId: req.body.userId,
+            rating: req.body.rating,
+            spoiler: req.body.spoiler,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }).then((result) => res.json(result));
+    });
+
+    //Get all posts of friends given a userId
+    app.get("/api/v1/posts/allFriends/:userId", (req, res) => {
+        let userId = req.params.userId;
+        // Get the users friends, store the ids in a list, and then get a list of posts using those ids
+        friendModule.getFriends(app, db, userId).then((friends) => {
+            if (!friends || !friends.length) {
+                res.json([]);
+                return;
+            }
+            var friendIds = friends.map(function(friend) {
+                return friend.user.id;
+            });
+            db.post.findAll({
+                where: {
+                    userId: {
+                        [Op.or]: friendIds
+                    }
+                },
+                include: [db.user]
+            }).then((result) => res.json(result));
+        });
+    });
+
+    //Get all posts of a given category
+    app.get("/api/v1/posts/categories/:cat", (req, res) => {
+        db.post.findAll({
+            where: {
+                category: req.params.cat
+            },
+            include: [db.user]
+        }).then((result) => res.json(result));
+    });
+
+    //Get all posts for a specified user
+    app.get("/api/v1/posts/user/:id", (req, res) => {
+        db.post.findAll({
+            where: {
+                userId: req.params.id
+            },
+            include: [db.user]
+        }).then((result) => res.json(result));
+    });
 }
